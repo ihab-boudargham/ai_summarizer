7a7a634986eb759326a08346fa4fd213b59219fb
ihab-boudargham <ihab.boudargham@hotmail.com>
2024-05-12 20:54:03 +0300

    Added Summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState([
@@ -9,10 +10,52 @@ const Demo = () => {
     },
   ]);
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  {
+    /* Save the history of all articles */
+  }
+  const [allArticles, setAllArticles] = useState([]);
+
+  // useEffect to save the history of all articles
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   // on submit we need to create a request
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = {
+        ...article,
+        summary: data.summary,
+      };
+
+      setArticle(newArticle);
+
+      const updatedArticles = [...allArticles, newArticle];
+      setAllArticles(updatedArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedArticles));
+
+      console.log(newArticle);
+    }
   };
+
+  {
+    /* getSummary will fetch the summarized data using the article url which will be set to article.url.
+  If the data is not null, we will create a new object, newArticle, which will take all the properties (Url which is not null (article.url) and summary that is null for now).
+  "summary: data.summary" will fill the summary property with the data that is returned from the API.  */
+  }
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -31,7 +74,7 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value={article.url}
+            value={article.url || ''}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
@@ -48,9 +91,62 @@ const Demo = () => {
         </form>
 
         {/* Browser URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src="/assets/copy.svg"
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img
+            src="/assets/loader.svg"
+            alt="loader"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen. Please try again later.
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div>
+              <div className="flex flex-col gap-3">
+                <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                  Article <span className="blue_gradient">Summary</span>
+                </h2>
+
+                <div className="summary_box">
+                  <p className="font-inter font-medium text-sm text-gray-700">
+                    {article.summary}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
